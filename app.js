5b108189702e7c607197d9d63aa058b2cc6c519d
Nicholette <nekonekonik@gmail.com>
2016-04-09 11:29:53 -0700

    cross site

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function(req, res) {
   res.type('text/plain'); // set content-type
   res.send('hello world'); // send text response
