80e49c2c6ce5362674267ed7c2d892d49878bb1b
Nicholette <nekonekonik@gmail.com>
2016-04-09 14:31:42 -0700

    add stars

--- client.js
+++ client.js
@@ -52,11 +52,47 @@ client.getMessageList = function(callback) {
 };
 
 function getTextMessage(message, callback) {
+	function parseStars(input) {
+		var numStar = 0;
+		var stringMatch;
+		var numberStar;
+
+		if (input.originalMessage.match(/[.*](\*)*\s*?$/) !== null){
+	  		starMatch = input.originalMessage.match(/[.*](\*)*\s*?$/)[0];
+			  if (starMatch === "*" ){
+			  	numStar = 1;
+			  }
+			   else if (starMatch === "**"){
+			  	numStar = 2;
+			  }
+			  else if (starMatch === "***"){
+			  	numStar = 3;
+			  }
+			  else if (starMatch === "****"){
+			  	numStar = 4;
+			  }
+			  else if (starMatch === "*****"){
+			  	numStar = 5;
+			  }
+			  else
+			  	numStar = 5;
+
+			  input.originalMessage = input.originalMessage.match(/.*?(?=\*)/)[0];
+			  input.translatedMessage = input.translatedMessage.match(/.*?(?=\*)/)[0];
+			  input.numStar = numStar;
+	 	 } else {
+		 	input.numStar = numStar;
+		  }
+		 callback(input);
+
+	}
+
+
 	var params = {
 	  text: message.body,
 	};
 
-var client = new MsTranslator({
+	var client = new MsTranslator({
       client_id: "Bighack_SG",
       client_secret: "Bighack_Singapore_92"
     });
@@ -73,13 +109,13 @@ var client = new MsTranslator({
           // console.log(data);
           // console.log(message);
           messageData = {
-              orignalMessage: message.body,
+              originalMessage: message.body,
               translatedMessage: data,
               messageTime: message.dateSent,
               sender: message.from
           };
 
-          callback(messageData);
+          parseStars(messageData);
         });
       });
   });
